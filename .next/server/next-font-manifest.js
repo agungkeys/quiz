1cc697136c26d20de0773b16d199acf265690dda
Agung Kurniawan <agung41149@gmail.com>
2024-04-20 19:07:47 +0800

    - add remove build files

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/_app\":[\"static/media/07a54048a9278940-s.p.woff2\"],\"/questions\":[\"static/media/07a54048a9278940-s.p.woff2\"]},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":true}"
